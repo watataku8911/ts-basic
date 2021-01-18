a0d0569fb17fc24e1b9bf0a33d5680d2e081731e
watataku8911 <watato.takuya@gmail.com>
2021-01-18 17:03:06 +0900

    first-commit 2021/01/17

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,13 @@
 // typeAliasSample()
 
 // 「06.配列とタプルの型定義」
-import arraySample from './array/array'
-import tupleSample from './array/tuple'
-arraySample()
-tupleSample()
+// import arraySample from './array/array'
+// import tupleSample from './array/tuple'
+// arraySample()
+// tupleSample()
+
+// 「07.ジェネリック型とポリモーフィズム」
+import genericsSample from './generics/basic'
+import advancedSample from './generics/advanced'
+genericsSample()
+advancedSample()
